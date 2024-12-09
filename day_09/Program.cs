ca0de6ee0e36d71e475294c784d607422d825f54
Daniel Gherghetta <gherghetta@gmail.com>
2024-12-09 12:41:34 +0100

    day 8

---
+++ day_09/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+string inString = "2333133121414131402";
+StringBuilder sb = new StringBuilder();
+for (int i = 0; i < inString.Length; i++)
+{
+    int amount = (int)char.GetNumericValue(inString[i]);
+    if (i % 2 == 0)
+    {
+        //fileblocks
+        sb.Append($"{i / 2}"[0], amount);
+    }
+    else
+    {
+        //empty spaces
+        sb.Append('.', amount);
+    }
+}
+
+int leftMostEmpty = GetLeftMostEmptyAfter(sb);
+for (int i = sb.Length - 1; i > 0; i--)
+{
+    char current = sb[i];
+    if (!char.IsDigit(current))
+        continue;
+
+    if( i <= leftMostEmpty)
+        break;
+
+    sb[leftMostEmpty] = current;
+    
+    sb[i] = '.';
+    Console.WriteLine(sb);
+
+    leftMostEmpty = GetLeftMostEmptyAfter(sb, leftMostEmpty);
+    if (leftMostEmpty == -1)
+        break;
+}
+
+
+int GetLeftMostEmptyAfter(StringBuilder sb, int i = -1)
+{
+    do
+    {
+        i++;
+        if(i == sb.Length)
+            return -1;
+    }
+    while (sb[i] != '.');
+
+    return i;
+}
+
+Console.WriteLine(sb);
