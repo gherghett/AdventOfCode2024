a743c6dc6996ac46c6fe4b18258f4a7da06fdcce
Daniel Gherghetta <gherghetta@gmail.com>
2024-12-20 13:49:00 +0100

    day 19 part 2

--- day_19/Program.cs
+++ day_19/Program.cs
@@ -14,27 +14,35 @@ inString = File.ReadAllText("input.txt");
 var available = inString.Trim().Split("\n\n")[0].Split(", ");
 var desiredPatterns = inString.Trim().Split("\n\n")[1].Split("\n");
 
-int howManyCanBeMade = desiredPatterns.Count(d => CanBeMade(d));
+var Ways = new Dictionary<string, long>();
 
-Console.WriteLine(howManyCanBeMade);
+long howManyCanBeMade = desiredPatterns.Count(d => WaysBeMade(d) > 0);
+Console.WriteLine($"Part 1: {howManyCanBeMade}");
 
-bool CanBeMade( string pattern, string from = "")
+long howManyWaysCanBeMade = desiredPatterns.Select(d => WaysBeMade(d)).Sum();
+Console.WriteLine($"PArt 2: {howManyWaysCanBeMade}");
+
+long WaysBeMade( string pattern)
 {
-    if( pattern == from )
-        return true;
+    if( pattern == "")
+        return 1;
+
+    if(Ways.ContainsKey(pattern))
+        return Ways[pattern];
     
-    var extentions = available.Select(s => from+s);
-    foreach( var ext in extentions)
+    int patternLength = pattern.Length;
+    
+    long ways = 0;
+    foreach( var ext in available.Where(s => s.Length <= patternLength))
     {
-        if( ext.Length > pattern.Length)
-            continue;
-        
         if( ext != pattern.Substring(0, ext.Length))
             continue;
-        
-        if (CanBeMade(pattern, ext) )
-            return true;
+
+        var rest = pattern.Substring(ext.Length, patternLength-ext.Length);
+
+        ways += WaysBeMade(rest);
     }
 
-    return false;
+    Ways[pattern] = ways;
+    return ways;
 }
